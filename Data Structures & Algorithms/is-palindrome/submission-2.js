cfb6c7d221d0ac1e840288ab7bf03f52034501df
Ramya Kamala Samudrala <112435495+samudraam@users.noreply.github.com>
2026-08-14 22:14:44 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        //clean the string
+        const noWhitespace = s.replace(/[^a-zA-Z0-9]/g, "");
+        const clean = noWhitespace.toLowerCase();
+
+        let left = 0;
+        let right = clean.length - 1;
+
+        while (left < right){
+            //see if the two are matches
+            if (clean[left] === clean[right]){
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+        //we sucsessfully got to the end of the while without returning false
+        return true;
+    }
+}
