04c8bda306d021555515796734436b7d1ab04e0c
Ramya Kamala Samudrala <112435495+samudraam@users.noreply.github.com>
2026-09-16 23:03:32 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,29 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else {
+            const currentMin = this.minStack[this.minStack.length - 1];
+            this.minStack.push(Math.min(val, currentMin));
+        }
+    }
+
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
